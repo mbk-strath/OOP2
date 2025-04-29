f0547e6b78b41306736fd917023bb7d69687ad86
mbk-strath <168066643+mbk-strath@users.noreply.github.com>
2025-04-29 09:46:40 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+        HelloTillStop obj = new HelloTillStop();  // Create an object of HelloTillStop
+        obj.showHello();  // Call the showHello method
+    }
+}
